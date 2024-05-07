ae5b149c48baf28fddabdd0fd7f27f37f54e95bc
7oooooda <7oooooda2017@gmail.com>
2024-05-07 21:50:41 +0300

    creating list view

--- lib/views/home_page_view.dart
+++ lib/views/home_page_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app_ui_setup/widgets/categories_listview.dart';
 import 'package:news_app_ui_setup/widgets/news_item_widget.dart';
+import 'package:news_app_ui_setup/widgets/news_listview.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,13 +26,7 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          CategoriesListView(),
-          SizedBox(height: 16,),
-          NewsItem(image:'https://unsplash.com/photos/two-white-vases-with-red-flowers-in-them-7T12an8w_hQ' ,title:  'dskfjdkfj fksdajfklds wekwl weew wejjrle eekewk sdfkl', discription: 'Hello'),
-        ],
-      ),
+      body: NewsListView(),
     );
   }
 }
---
+++ lib/widgets/news_listview.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app_ui_setup/widgets/news_item_widget.dart';
+
+class NewsListView extends StatelessWidget {
+  const NewsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (contect, index) {
+        return NewsItem(
+          image:
+              'https://unsplash.com/photos/two-white-vases-with-red-flowers-in-them-7T12an8w_hQ',
+          title: 'dskfjdkfj fksdajfklds wekwl weew wejjrle eekewk sdfkl',
+          discription: 'Hello',
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(height: 24.h),
+      itemCount: 10,
+    );
+  }
+}
